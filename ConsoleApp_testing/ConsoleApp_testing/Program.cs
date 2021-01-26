771c06c895089c417c009ee7a3972699f846ce49
Alexei001 <alexeiyoyo12@gmail.com>
2021-01-26 08:37:56 +0200

    C(sharp)_projects

---
+++ ConsoleApp_testing/ConsoleApp_testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ConsoleApp_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.WriteLine("salut lume!");
+            string data;
+            data = Console.ReadLine();
+            Console.WriteLine("data in variable "+ data);
+            
+        }
+    }
+}
