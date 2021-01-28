9fd2dcee26e726555a6b559a2e49bcdce213f6dd
Alexei001 <alexeiyoyo12@gmail.com>
2021-01-29 01:26:00 +0200

    new_details_add

--- ConsoleApp_testing/ConsoleApp_testing/Program.cs
+++ ConsoleApp_testing/ConsoleApp_testing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 
 namespace ConsoleApp_testing
@@ -7,12 +8,45 @@ namespace ConsoleApp_testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
-            Console.WriteLine("salut lume!");
-            string data;
-            data = Console.ReadLine();
-            Console.WriteLine("data in variable "+ data);
-            
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo() {
+                NumberDecimalSeparator = ".",
+            };
+            /* string str,str2,str3;
+             int num1, num2, sum;
+             str2 = "1.45";
+             str3 = "6";
+
+
+             Console.WriteLine("paste number 1");
+              str = Console.ReadLine();
+             num1 = Convert.ToInt32(str);
+
+             Console.WriteLine("paste number 2");
+             str = Console.ReadLine();
+             num2 = Convert.ToInt32(str);
+
+             Console.WriteLine("sum is:");
+             sum = num1 + num2;
+             Console.WriteLine(sum);
+
+             double doubleNum1=Convert.ToDouble(str2,numberFormatInfo);
+             int doubleNum2 = Convert.ToInt32(str3);
+             double doubleSum = doubleNum1 + doubleNum2;
+
+             Console.WriteLine("double num:" + doubleSum);*/
+
+            /*parsing number to convert*/
+
+
+            string str="1,6";
+
+            double num = double.Parse(str, numberFormatInfo);
+
+            Console.WriteLine("num : "+num);
+            Console.WriteLine(num);
+
+
+
         }
     }
 }
